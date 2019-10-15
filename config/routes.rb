40d2d1b0d11c23941cc232e6a261179f5c9b9ab5
clalil <cliljander@gmail.com>
2019-10-15 19:03:19 +0200

    Delete old test controller, routes and spec. Updates routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    namespace :v0 do
-      resources :pings, only: [:index], constraints: { format: 'json' }
+    mount_devise_token_auth_for 'User', at: 'auth'
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
 end
