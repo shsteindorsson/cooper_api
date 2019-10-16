a739e4f3111036446fe13357a68ff12452ccd208
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-16 10:11:14 +0200

    Add user authentication methods to Performance Data controller

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,3 +1,20 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  before_action :authenticate_api_v1_user!
+  
+  def create
+    @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
 
+    if @data.save
+      render json: { message: 'all good' }
+    else
+      render json: { error: @data.errors.full_messages }
+    end
+  end
+
+  private
+
+  def performance_data_params
+    params.require(:performance_data).permit!
+    # A better example: params.require(:performance_data).permit(:user, :data)
+  end
 end
